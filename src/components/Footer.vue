68dbe6e2e959f212552c7a5d505f70aa565e7305
iAngelManuel <iangelmanuel@hotmail.com>
2023-07-09 12:09:06 -0500

    second commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-gradient-to-br from-purple-900 to-indigo-800 text-white py-4 mt-52">
+  <footer class="bg-gradient-to-br from-purple-800 to-indigo-900 text-white py-4 mt-52">
     <div class="container mx-auto text-center">
       <p class="text-sm">© {{new Date().getFullYear()}} Tu Compañía. Todos los derechos reservados.</p>
     </div>
